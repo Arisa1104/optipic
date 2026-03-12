94c37111e5113e68f5ffcb1fba612e4046a51c6b
Arisa <arisa1104@aol.com>
2026-03-12 19:53:05 -0400

    fix: update siteOrigin for vercel deployments and point default to optipic.app

--- src/static-build/utils.tsx
+++ src/static-build/utils.tsx
@@ -66,11 +66,14 @@ export function escapeStyleScriptContent(str: string): string {
  */
 export const siteOrigin = (() => {
   if (process.env.DEV_PORT) return `http://localhost:${process.env.DEV_PORT}`;
+  if (process.env.VERCEL_PROJECT_PRODUCTION_URL)
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   // https://docs.netlify.com/configure-builds/environment-variables/#build-metadata
-  if (process.env.CONTEXT === 'production') return 'https://squoosh.app';
+  if (process.env.CONTEXT === 'production') return 'https://optipic.app';
   if (process.env.DEPLOY_PRIME_URL) return process.env.DEPLOY_PRIME_URL;
   console.warn(
-    'Unable to determine site origin, defaulting to https://squoosh.app',
+    'Unable to determine site origin, defaulting to https://optipic.app',
   );
-  return 'https://squoosh.app';
+  return 'https://optipic.app';
 })();
